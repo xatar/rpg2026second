eaa5e7920022d4af66465466cb6136aeca0cf55b
xaman <pavel.turnhofer@seznam.cz>
2026-04-22 20:02:46 +0200

    npc patrol behaviour

--- npc_behaviour_patrol.gd
+++ npc_behaviour_patrol.gd
@@ -1,11 +1,90 @@
-extends Node2D
+@tool
+extends NPCBehaviour
+
+const COLORS = [Color(1,0,0),Color(0,1,0),Color(0,0,1),Color(1,1,0),Color(1,0,1),Color(0,1,1),Color(1,0,0)]
+
+@export var walk_speed : float = 30.0
+var patrol_location : Array[PatrolLocation]
+var current_location_index : int = 0
+var target : PatrolLocation
+
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	gather_patrol_locations()
+	if Engine.is_editor_hint():
+		child_entered_tree.connect(gather_patrol_locations)
+		child_order_changed.connect(gather_patrol_locations)
+		return
 	pass # Replace with function body.
+	super()
+	if patrol_location.size() == 0:
+		process_mode = Node.PROCESS_MODE_DISABLED
+		return
+	target = patrol_location[0]
 
+func gather_patrol_locations(_n : Node = null)->void:
+	patrol_location = []
+	for c in get_children():
+		if c is PatrolLocation:
+			patrol_location.append(c)
+			
+	if Engine.is_editor_hint():
+		if patrol_location.size()>0:
+			for i in patrol_location.size():
+				var _p = patrol_location[i] as PatrolLocation
+				
+				#if not _p.transform_changed.is_connected(gather_patrol_locations):
+					#_p.transform_changed.connect(gather_patrol_locations)
+				
+				_p.update_label(str(i))
+				_p.modulate = _get_color_by_index(i)
+				
+				#var _next : PatrolLocation
+				#if i < patrol_location.size()-1:
+					#_next = patrol_location[i + 1]
+				#else:
+					#_next = patrol_location[0]
+				#_p.update_line(_next.postion)
+		return
+	pass
+	
+func _get_color_by_index(i : int )->Color:
+	var color_count : int = COLORS.size()
+	while i > color_count -1:
+		i -= color_count
+	return COLORS[i]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		return
+	if npc.global_position.distance_to(target.target_postion) < 1:
+		start()
+	pass
+	
+func start()->void:
+	if npc.do_behaviour == false or patrol_location.size()<2:
+		return
+	#Idle 
+	npc.global_position = target.target_postion
+	npc.state = "idle"
+	npc.velocity = Vector2.ZERO
+	npc.update_animation()
+	var wait_time : float = target.wait_time
+	current_location_index += 1
+	if current_location_index >= patrol_location.size():
+		current_location_index = 0
+	target = patrol_location[current_location_index]
+	await get_tree().create_timer(wait_time).timeout
+	if npc.do_behaviour == false:
+		return
+	npc.state = "walk"
+	var _dir = global_position.direction_to(target.target_postion)
+	npc.direction = _dir
+	npc.velocity = walk_speed * _dir
+	npc.update_direction(target.target_postion)
+	npc.update_animation()
 	pass
--- patrol_location.gd
+++ patrol_location.gd
@@ -7,10 +7,21 @@ class_name PatrolLocation extends Node2D
 		_update_wait_time_label()
 		
 		
+var target_postion : Vector2  = Vector2.ZERO
+
+signal transform_changed
+
+func _enter_tree()->void:
+	set_notify_transform(true)
+	
+	
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_TRANSFORM_CHANGED:
+		transform_changed.emit()
 
 func _update_wait_time_label()->void:
 	if Engine.is_editor_hint():
-		$Sprite2D/Label2.text = "wait: " + str(wait_time) + "s"
+		$Sprite2D/Label2.text = "wait: " + str( wait_time ) + "s"
 		 
 	pass
 
@@ -18,9 +29,22 @@ func _update_wait_time_label()->void:
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	target_postion = global_position
+	_update_wait_time_label()
+	if Engine.is_editor_hint():
+		return
+	$Sprite2D.queue_free()
+	
+	
 	pass # Replace with function body.
 
-
+func update_label(_s:String)->void:
+	$Sprite2D/Label.text = _s
+	
+func update_line(next_location:Vector2)->void:
+	var line : Line2D = $Sprite2D/Line2D
+	line.points[1] = next_location - position
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
